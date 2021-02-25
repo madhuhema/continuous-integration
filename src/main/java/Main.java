7bbfc5c8a5ef7a965e5c6146385fae21d5e5e690
Madhuvanthi Hemanathan <madhunarmi@gmail.com>
2021-02-25 13:54:06 +0530

    added maven for release

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,24 @@
+/**
+ * Main class
+ */
 public class Main {
+    /**
+     * local variable
+     */
     String name = "hello";
-
+    /**
+     * static main method
+     * @param args arguments
+     */
     public static void main(String[] args) {
         Main main = new Main();
         main.main();
         System.out.println(main.name);
     }
+
+    /**
+     * main method
+     */
     public void main() {
         name = "changed";
     }
